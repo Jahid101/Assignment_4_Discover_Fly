82bd482874d90e82836342fb878484191de1d1ab
Jahid101 <jahidhasananik.official@gmail.com>
2021-01-30 05:01:40 +0600

    All done

--- script.js
+++ script.js
@@ -50,13 +50,25 @@ function plusMinusIncreaseDecrease(ticket, id, increase) {
     }
 }
 
-function calculation(){
+function calculation() {
     let subtotalValue = (document.getElementById('firstClassTicketNumber').value * 150) + (document.getElementById('economyTicketNumber').value * 100);
-            document.getElementById('subtotal').innerText = subtotalValue;
+    document.getElementById('subtotal').innerText = subtotalValue;
 
-            let vat = subtotalValue * 0.1;
-            document.getElementById('vat').innerText = vat;
+    let vat = subtotalValue * 0.1;
+    document.getElementById('vat').innerText = vat;
 
-            let total = subtotalValue + vat;
-            document.getElementById('total').innerText = total;
+    let total = subtotalValue + vat;
+    document.getElementById('total').innerText = total;
 }
+
+const bookingBtn = document.getElementById('bookingBtn').addEventListener('click', function () {
+    document.getElementById('show').style.display = 'none';
+
+    document.getElementById('hide').style.display = 'block';
+
+    document.getElementById('showFirstClassTicketNumber').innerText = document.getElementById('firstClassTicketNumber').value;
+    document.getElementById('showEconomyClassTicketNumber').innerText = document.getElementById('economyTicketNumber').value;
+    document.getElementById('showSubtotal').innerText = document.getElementById('subtotal').innerText;
+    document.getElementById('showVat').innerText = document.getElementById('vat').innerText;
+    document.getElementById('showTotal').innerText = document.getElementById('total').innerText;
+})
